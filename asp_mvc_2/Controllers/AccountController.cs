d28707bd86a2e077bd3da36e8ec20b4d0962cb69
Salsa <salsabillamegasavira@gmail.com>
2017-01-31 10:50:07 +0700

    Add project files.

---
+++ asp_mvc_2/Controllers/AccountController.cs
@@ -0,0 +1,33 @@
+﻿using asp_mvc_2.Models.EntityManager;
+using asp_mvc_2.Models.ViewModel;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace asp_mvc_2.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult SignUp(UserSignUpView USV)
+        {
+            if (ModelState.IsValid)
+            {
+                UserManager UM = new UserManager();
+                if (!UM.IsLoginNameExist(USV.LoginName))
+                {
+                    UM.AddUserAccount(USV);
+                    FormsAuthentication.SetAuthCookie(USV.FirstName, false);
+                    return RedirectToAction("Welcome", "Home");
+                }
+                else
+                    ModelState.AddModelError("", "Login Name already taken.");
+            }
+            return View();
+        }
+    }
+}
